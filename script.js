4d69036051040d3a531c5f9d7ba76db85505a94b
AbhaySoam123 <127785616+AbhaySoam123@users.noreply.github.com>
2023-09-05 11:38:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 // .................../ JS Code for smooth scrolling /...................... -->
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
 //   <script>
     $(document).ready(function () {
       // Add smooth scrolling to all links
